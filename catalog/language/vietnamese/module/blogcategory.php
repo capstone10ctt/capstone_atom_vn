9a6ac876f98139bfc921a419719ddedb43aa2ef1
trinhhuuduc <thuuduc@apcs.vn>
2014-01-02 10:46:29 +0700

    vietnamese translation 2nd time

---
+++ catalog/language/vietnamese/module/blogcategory.php
@@ -0,0 +1,5 @@
+<?php
+// Heading
+$_['heading_title'] = 'Thông báo';
+$_['text_bosblog'] = 'Các thông báo';
+?>
---
+++ catalog/language/vietnamese/module/blogtagcloud.php
@@ -0,0 +1,13 @@
+<?php
+//-----------------------------------------------------
+// TagCloud for Opencart v1.5.3    
+// Created by villagedefrance                          
+// contact@villagedefrance.net                                    
+//-----------------------------------------------------
+
+// Heading
+$_['heading_title']  	= 'Từ khóa thông dụng';
+
+// Messages
+$_['text_notags']  		= 'Chưa có từ khóa nào';
+?>
