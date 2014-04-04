d79a07474c084ecd278f701d9f7ea949cd8d7dea
Dinh Hai Nguyen <dhnguyenit@gmail.com>
2014-04-04 11:05:07 +0700

    fix manage_wie

--- admin/model/price/standard.php
+++ admin/model/price/standard.php
@@ -10,7 +10,7 @@ class ModelPriceStandard extends Model {
 	//end vlmn modification
     
     public function getElectricityStandardPrice($id) {
-        $query = $this->db->query('SELECT `From`, `To`, Price FROM e_standard WHERE id = "' . $id . '"');
+        $query = $this->db->query('SELECT `From`, `To`, Price FROM e_standard WHERE id = "' . $id . '" ORDER BY `From`');
         return $query->rows;
     }
 
@@ -38,7 +38,7 @@ class ModelPriceStandard extends Model {
     }
 	//end vlmn modification
     public function getWaterStandardPrice($id) {
-        $query = $this->db->query('SELECT `From`, `To`, Price FROM w_standard WHERE id = "' . $id . '"');
+        $query = $this->db->query('SELECT `From`, `To`, Price FROM w_standard WHERE id = "' . $id . '" ORDER BY `From`');
         return $query->rows;
     }
 
