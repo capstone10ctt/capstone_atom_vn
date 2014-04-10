bb6063a4c209a2bff82906c9876cd2b8f1fd0534
Võ Lý Minh Nhân <volyminhnhan@gmail.com>
2014-04-11 04:27:12 +0700

    update manage WIE 6

--- admin/controller/tool/import.php
+++ admin/controller/tool/import.php
@@ -439,16 +439,14 @@ class ControllerToolImport extends Controller {
 					$data['electric_usage'][] = $temp_elec;
 					$data['water_usage'][] = $temp_water;
 				
-			       if($this->session->data['col_addeddate'] != '') {
-					   if($this->session->data['sheetData'][$i][$this->session->data['col_addeddate']] != '') {
-						   //echo $this->session->data['sheetData'][$i][$this->session->data['col_addeddate']].'<br/>';
-						   $d = DateTime::createFromFormat('d/m/Y', $this->session->data['sheetData'][$i][$this->session->data['col_addeddate']]);
-						   //$my_date = date('d-m-Y', strtotime($this->session->data['col_addeddate']));
-							$data['year'] = $d->format('Y');
-							$data['month'] = $d->format('m');
-							$is_set = true;
-					   }
-					}
+			       if($this->session->data['sheetData'][$i][$this->session->data['col_addeddate']] != '') {
+					   //echo $this->session->data['sheetData'][$i][$this->session->data['col_addeddate']].'<br/>';
+					   $d = DateTime::createFromFormat('d/m/Y', $this->session->data['sheetData'][$i][$this->session->data['col_addeddate']]);
+					   //$my_date = date('d-m-Y', strtotime($this->session->data['col_addeddate']));
+						$data['year'] = $d->format('Y');
+						$data['month'] = $d->format('m');
+						$is_set = true;
+				   }
 					else if(!$is_set){
 						$data['year'] = date('Y');
 						$data['month'] = date('m');
